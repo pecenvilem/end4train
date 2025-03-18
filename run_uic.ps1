626fa033d03d0ac9e490aed9b3f4d5bfc95528ee
Vilém Pecen <pecenvilem@gmail.com>
2025-03-18 19:17:28 +0100

    Temporary commit

--- run_uic.ps1
+++ run_uic.ps1
@@ -1,3 +1,1 @@
-$VIRTUAL_ENV = poetry env info --path
-$UIC_CALL = "$VIRTUAL_ENV\Lib\site-packages\PySide6\uic.exe -g python -o src/end4train/ui/main_window_ui.py qt_designer/monitor_main_window.ui"
-Invoke-Expression $UIC_CALL
+pyside6-uic.exe -g python -o src/end4train/ui/main_window_ui.py qt_designer/monitor_main_window.ui
