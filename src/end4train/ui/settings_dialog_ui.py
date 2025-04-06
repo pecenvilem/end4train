98c1232f3ab0d743634ad2f956481687855bc2c2
Vilém Pecen <pecenvilem@gmail.com>
2025-04-06 22:39:49 +0200

    Temporary commit

--- src/end4train/ui/settings_dialog_ui.py
+++ src/end4train/ui/settings_dialog_ui.py
@@ -15,54 +15,36 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractButton, QApplication, QComboBox, QDialog,
-    QDialogButtonBox, QFormLayout, QLabel, QSizePolicy,
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
+    QFormLayout, QHeaderView, QSizePolicy, QTreeView,
     QWidget)
 
 class Ui_SettingsDialog(object):
     def setupUi(self, SettingsDialog):
         if not SettingsDialog.objectName():
             SettingsDialog.setObjectName(u"SettingsDialog")
-        SettingsDialog.resize(215, 98)
+        SettingsDialog.resize(403, 201)
         SettingsDialog.setSizeGripEnabled(False)
-        SettingsDialog.setModal(False)
+        SettingsDialog.setModal(True)
         self.formLayout = QFormLayout(SettingsDialog)
         self.formLayout.setObjectName(u"formLayout")
-        self.label = QLabel(SettingsDialog)
-        self.label.setObjectName(u"label")
-
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label)
-
-        self.style_combobox = QComboBox(SettingsDialog)
-        self.style_combobox.setObjectName(u"style_combobox")
-
-        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.style_combobox)
-
-        self.label_2 = QLabel(SettingsDialog)
-        self.label_2.setObjectName(u"label_2")
-
-        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label_2)
-
-        self.color_override_combobox = QComboBox(SettingsDialog)
-        self.color_override_combobox.addItem("")
-        self.color_override_combobox.addItem("")
-        self.color_override_combobox.addItem("")
-        self.color_override_combobox.setObjectName(u"color_override_combobox")
-
-        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.color_override_combobox)
-
         self.buttonBox = QDialogButtonBox(SettingsDialog)
         self.buttonBox.setObjectName(u"buttonBox")
         self.buttonBox.setOrientation(Qt.Orientation.Horizontal)
-        self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Save)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Apply|QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.RestoreDefaults|QDialogButtonBox.StandardButton.Save)
         self.buttonBox.setCenterButtons(False)
 
-        self.formLayout.setWidget(2, QFormLayout.SpanningRole, self.buttonBox)
+        self.formLayout.setWidget(1, QFormLayout.SpanningRole, self.buttonBox)
 
-#if QT_CONFIG(shortcut)
-        self.label.setBuddy(self.style_combobox)
-        self.label_2.setBuddy(self.color_override_combobox)
-#endif // QT_CONFIG(shortcut)
+        self.treeView = QTreeView(SettingsDialog)
+        self.treeView.setObjectName(u"treeView")
+        self.treeView.setTabKeyNavigation(True)
+        self.treeView.setAlternatingRowColors(True)
+        self.treeView.header().setCascadingSectionResizes(True)
+        self.treeView.header().setStretchLastSection(False)
+
+        self.formLayout.setWidget(0, QFormLayout.SpanningRole, self.treeView)
+
 
         self.retranslateUi(SettingsDialog)
         self.buttonBox.accepted.connect(SettingsDialog.accept)
@@ -73,11 +55,5 @@ class Ui_SettingsDialog(object):
 
     def retranslateUi(self, SettingsDialog):
         SettingsDialog.setWindowTitle(QCoreApplication.translate("SettingsDialog", u"Settings", None))
-        self.label.setText(QCoreApplication.translate("SettingsDialog", u"Style", None))
-        self.label_2.setText(QCoreApplication.translate("SettingsDialog", u"Override Color Scheme", None))
-        self.color_override_combobox.setItemText(0, QCoreApplication.translate("SettingsDialog", u"None", None))
-        self.color_override_combobox.setItemText(1, QCoreApplication.translate("SettingsDialog", u"Light", None))
-        self.color_override_combobox.setItemText(2, QCoreApplication.translate("SettingsDialog", u"Dark", None))
-
     # retranslateUi
 
